79304e92e9482479e6d557a3c64d5406f39ba454
Aman Suman <your-github-email@gmail.com>
2026-06-11 06:01:03 +0530

    Railway Database Setup

--- db.js
+++ db.js
@@ -2,13 +2,13 @@ const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
 
-    host: mysql.railway.internal,
+    host: "mysql.railway.internal",
 
-    user: root,
+    user: "root",
 
-    password: klGioSnnVncaHSTALBDDpxrOAGimhyrz,
+    password: "klGioSnnVncaHSTALBDDpxrOAGimhyrz",
 
-    database: railway,
+    database: "railway",
 
     port: 3306
 
@@ -18,9 +18,7 @@ connection.connect((err) => {
 
     if(err){
 
-        console.log(
-            "Database Connection Failed"
-        );
+        console.log(err);
 
     }
     else{
